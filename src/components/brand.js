d871a766c6a6d7dc55ada598371ea97197528b2a
Nestor <elnestinatorHD@gmail.com>
2020-07-25 20:36:16 +0200

    Added new icons and fixed the logo issue.

--- src/components/brand.js
+++ src/components/brand.js
@@ -4,7 +4,7 @@ import { Flex, Image, Box, Heading, Text } from "@chakra-ui/core";
 export function Brand() {
   return (
     <Flex alignItems="center">
-      <Image src="logo.png" onError={console.log("Error")} />
+      <Image src={process.env.PUBLIC_URL + '/logo.svg'}/>
       <Box ml="10px">
         <Heading fontSize="24px">GitHunter</Heading>
         <Text color="gray.600">Starred project hunter for Github</Text>
