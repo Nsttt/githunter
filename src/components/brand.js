f55bb30a700ee01374ec067c85d233807cd798ff
Nestor <elnestinatorHD@gmail.com>
2021-01-21 18:51:33 +0100

    Fixed logo.

--- src/components/brand.js
+++ src/components/brand.js
@@ -4,7 +4,7 @@ import { Flex, Image, Box, Heading, Text } from "@chakra-ui/core";
 export function Brand() {
   return (
     <Flex alignItems="center">
-      <Image src={process.env.PUBLIC_URL + '/logo.svg'}/>
+      <Image src={`${process.env.PUBLIC_URL}/logo.svg`} height="65px" width="75px"/>
       <Box ml="10px">
         <Heading fontSize="24px">GitHunter</Heading>
         <Text color="gray.600">Starred project hunter for Github</Text>
